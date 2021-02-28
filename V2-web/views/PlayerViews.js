5d438afb8e85c786d82e8ca89f399ad12c80e745
squiddible <63127872+squidKid-deluxe@users.noreply.github.com>
2021-02-28 18:41:42 -0500

    Update PlayerViews.js

--- V2-web/views/PlayerViews.js
+++ V2-web/views/PlayerViews.js
@@ -7,6 +7,9 @@ const exports = {};
 // It does not have its own Wrapper view. //
 ////////////////////////////////////////////
 
+
+
+
 /////////////////////////////////////
 // Page for winning/losing message //
 /////////////////////////////////////
@@ -16,7 +19,7 @@ exports.Finale = class extends React.Component {
         const { won } = this.props;
         return (
             <div>
-                <h1>{!won ? "YOU WIN!" : "YOU LOSE!"}</h1>
+              <h1>{!won ? "YOU WIN!" : "YOU LOSE!"}</h1>
             </div>
         );
     }
@@ -28,48 +31,39 @@ exports.Finale = class extends React.Component {
 
 exports.GetHandb = class extends React.Component {
     render() {
-        const { parent } = this.props;
+        const {parent} = this.props;
         return (
-            <div class="container">
-                Waiting for the other player...
-                <center>
-                    <img src="https://imgur.com/CsxXjde.png" width="900px" height="720px" style={{ zIndex: 0, position: "absolute", top: "0px", left: "50%", marginLeft: "-450px" }}></img>
-                </center>
-                <center>
-                    <img src="https://imgur.com/Hla2Ybx.png" width="900px" height="720px" style={{ zIndex: 1, position: "absolute", top: "0px", left: "50%", marginLeft: "-450px" }}></img>
-                </center>
-                <center>
-                    <div style={{ zIndex: 2, position: "absolute", top: "145px", left: "50%", marginLeft: "-800px" }}>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box7" id="box7"></img>
-                            <pre> </pre>
-                            <img class="box8" id="box8"></img>
-                            <pre> </pre>
-                            <img class="box9" id="box9"></img>
-                            <pre></pre>
-                        </div>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box4" id="box4"></img>
-                            <pre> </pre>
-                            <img class="box5" id="box5"></img>
-                            <pre> </pre>
-                            <img class="box6" id="box6"></img>
-                            <pre></pre>
-                        </div>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box1" id="box1"></img>
-                            <pre> </pre>
-                            <img class="box2" id="box2"></img>
-                            <pre> </pre>
-                            <img class="box3" id="box3"></img>
-                            <pre></pre>
-                        </div>
-                    </div>
-                </center>
+<div class="container">
+Waiting for the other player...
+        <center><img src="https://imgur.com/CsxXjde.png" width="900px" height="720px" style={{zIndex:0, position:"absolute", top:"0px", left:"50%", marginLeft:"-450px"}}></img></center>
+        <center><img src="https://imgur.com/Hla2Ybx.png" width="900px" height="720px" style={{zIndex:1, position:"absolute", top:"0px", left:"50%", marginLeft:"-450px"}}></img></center>
+        <center><div style={{zIndex:2, position:"absolute", top:"145px", left:"50%",  marginLeft:"-800px"}}>
+           
+            <div class="drop-targets">
+                <pre>                                     </pre>
+                <img class="box7" id="box7"></img>
+                <pre>             </pre><img class="box8" id="box8"></img>
+                <pre>             </pre><img class="box9" id="box9"></img>
+                <pre></pre>
             </div>
+            <div class="drop-targets">
+                <pre>                            </pre>
+                <img class="box4" id="box4"></img>
+                <pre>         </pre><img class="box5" id="box5"></img>
+                <pre>         </pre><img class="box6" id="box6"></img>
+                <pre></pre>
+            </div>
+            <div class="drop-targets">
+                <pre>                 </pre>
+                <img class="box1" id="box1"></img>
+                <pre>      </pre><img class="box2" id="box2"></img>
+                <pre>      </pre><img class="box3" id="box3"></img>
+                <pre></pre>
+            </div>
+            
+        </div></center>
+        
+    </div>
         );
     }
 };
@@ -80,47 +74,38 @@ exports.GetHandb = class extends React.Component {
 
 exports.GetHand = class extends React.Component {
     render() {
-        const { parent } = this.props;
+        const {parent} = this.props;
         return (
-            <div class="container">
-                <center>
-                    <img src="https://imgur.com/CsxXjde.png" width="900px" height="720px" style={{ zIndex: 0, position: "absolute", top: "300px", left: "50%", marginLeft: "-450px" }}></img>
-                </center>
-                <center>
-                    <img src="https://imgur.com/Hla2Ybx.png" width="900px" height="720px" style={{ zIndex: 1, position: "absolute", top: "300px", left: "50%", marginLeft: "-450px" }}></img>
-                </center>
-                <center>
-                    <div style={{ zIndex: 2, position: "absolute", top: "445px", left: "50%", marginLeft: "-500px" }}>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box7" onclick={() => parent.setimg("box7")} id="box7"></img>
-                            <pre> </pre>
-                            <img class="box8" onclick={() => parent.setimg("box8")} id="box8"></img>
-                            <pre> </pre>
-                            <img class="box9" onclick={() => parent.setimg("box9")} id="box9"></img>
-                            <pre></pre>
-                        </div>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box4" onclick={() => parent.setimg("box4")} id="box4"></img>
-                            <pre> </pre>
-                            <img class="box5" onclick={() => parent.setimg("box5")} id="box5"></img>
-                            <pre> </pre>
-                            <img class="box6" onclick={() => parent.setimg("box6")} id="box6"></img>
-                            <pre></pre>
-                        </div>
-                        <div class="drop-targets">
-                            <pre> </pre>
-                            <img class="box1" onclick={() => parent.setimg("box1")} id="box1"></img>
-                            <pre> </pre>
-                            <img class="box2" onclick={() => parent.setimg("box2")} id="box2"></img>
-                            <pre> </pre>
-                            <img class="box3" onclick={() => parent.setimg("box3")} id="box3"></img>
-                            <pre></pre>
-                        </div>
-                    </div>
-                </center>
+        <div class="container">
+        <center><img src="https://imgur.com/CsxXjde.png" width="900px" height="720px" style={{zIndex:0, position:"absolute", top:"300px", left:"50%", marginLeft:"-450px"}}></img></center>
+        <center><img src="https://imgur.com/Hla2Ybx.png" width="900px" height="720px" style={{zIndex:1, position:"absolute", top:"300px", left:"50%", marginLeft:"-450px"}}></img></center>
+        <center><div style={{zIndex:2, position:"absolute", top:"445px", left:"50%",  marginLeft:"-500px"}}>
+           
+            <div class="drop-targets">
+                <pre>                                     </pre>
+                <img class="box7" onclick={() => parent.setimg('box7')} id="box7"></img>
+                <pre>             </pre><img class="box8" onclick={() => parent.setimg('box8')} id="box8"></img>
+                <pre>             </pre><img class="box9" onclick={() => parent.setimg('box9')} id="box9"></img>
+                <pre></pre>
             </div>
+            <div class="drop-targets">
+                <pre>                            </pre>
+                <img class="box4" onclick={() => parent.setimg('box4')} id="box4"></img>
+                <pre>         </pre><img class="box5" onclick={() => parent.setimg('box5')} id="box5"></img>
+                <pre>         </pre><img class="box6" onclick={() => parent.setimg('box6')} id="box6"></img>
+                <pre></pre>
+            </div>
+            <div class="drop-targets">
+                <pre>                 </pre>
+                <img class="box1" onclick={() => parent.setimg('box1')} id="box1"></img>
+                <pre>      </pre><img class="box2" onclick={() => parent.setimg('box2')} id="box2"></img>
+                <pre>      </pre><img class="box3" onclick={() => parent.setimg('box3')} id="box3"></img>
+                <pre></pre>
+            </div>
+            
+        </div></center>
+        
+    </div>
         );
     }
 };
